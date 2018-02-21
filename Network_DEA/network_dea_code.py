c5f37e5a8eff4144cf92dd3f9ef69abedbc3ee2d
wurmen <ftcu5931@gmail.com>
2018-02-21 16:24:15 +0800

    update

--- Network_DEA/network_dea_code.py
+++ Network_DEA/network_dea_code.py
@@ -1,8 +1,11 @@
 from gurobipy import*
+
+
 DMU=['A', 'B','C','D','E']
 E={}
 val_p1,val_p2,val_p3,val_s1,val_s2={},{},{},{},{}
 slack_p1,slack_p2,slack_p3={},{},{}
+
 for k in DMU:
 
     I=2
@@ -12,12 +15,12 @@ for k in DMU:
     DMU,proc1x1,proc1x2,proc1TotyO,proc1yO,proc1yI=multidict({("A"):[3,5,4,2,2],("B"):[2,3,2,1,1],("C"):[3,4,2,1,1],("D"):[4,6,3,2,1],("E"):[5,6,4,3,1]})
     DMU,proc2x1,proc2x2,proc2TotyO,proc2yO,proc2yI=multidict({("A"):[4,3,3,2,1],("B"):[2,1,2,1,1],("C"):[5,3,2,1,1],("D"):[5,5,4,3,1],("E"):[5,4,4,2,2]})
     DMU,proc3x1,proc3x2,proc3TotyO=multidict({("A"):[4,6,1],("B"):[3,3,1],("C"):[3,7,2],("D"):[5,3,1],("E"):[4,5,3]})
-    P1={}
-    P2={}
-    P3={}
-    v={}
-    u={}
+    
+    P1,P2,P3={},{},{}
+    v,u={},{}
+    
     m=Model("network_DEA")
+    
     for i in range(I):
         v[i]=m.addVar(vtype=GRB.CONTINUOUS,name="v_%d"%i)
     
@@ -40,10 +43,12 @@ for k in DMU:
     
     u_sol = m.getAttr('x', u)
     v_sol = m.getAttr('x',v)
+    
     #計算各process的效率值
     E1=u_sol[0]*proc1TotyO[k]/(v_sol[0]*proc1x1[k]+v_sol[1]*proc1x2[k]) 
     E2=u_sol[1]*proc2TotyO[k]/(v_sol[0]*proc2x1[k]+v_sol[1]*proc2x2[k])
     E3=u_sol[2]*proc3TotyO[k]/(v_sol[0]*proc3x1[k]+v_sol[1]*proc3x2[k]+u_sol[0]*proc1yI[k]+u_sol[1]*proc2yI[k])
+    
     #計算各stage的效率值
     stage1=(u_sol[0]*proc1TotyO[k]+u_sol[1]*proc2TotyO[k]+v_sol[0]*proc3x1[k]+v_sol[1]*proc3x2[k])/(v_sol[0]*Totx1[k]+v_sol[1]*Totx2[k])
     stage2=(u_sol[0]*proc1yO[k]+u_sol[1]*proc2yO[k]+u_sol[2]*proc3TotyO[k])/(u_sol[0]*proc1TotyO[k]+u_sol[1]*proc2TotyO[k]+v_sol[0]*proc3x1[k]+v_sol[1]*proc3x2[k])
@@ -52,6 +57,7 @@ for k in DMU:
     val_p3[k]='The efficiency of process 3 of DMU %s:%4.4g'%(k,E3)
     val_s1[k]='The efficiency of stage 1 of DMU %s:%4.4g'%(k,stage1)
     val_s2[k]='The efficiency of stage 2 of DMU %s:%4.4g'%(k,stage2)
+    
     #顯示各process的無效率值
     process1_slack=m.getAttr('slack',P1)
     slack_p1[k]='The inefficiency of process 1 of DMU %s:%4.4g'%(k,process1_slack[k])
